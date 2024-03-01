92aa4f89c867983c4895a8638bc9d7fbe5b4acc5
amiralimbekerov <asuraaka@outlook.com>
2024-03-01 14:22:59 +0500

    add google map

--- app/src/main/java/com/example/crimeguardian/IncidentsFragment.kt
+++ app/src/main/java/com/example/crimeguardian/IncidentsFragment.kt
@@ -5,15 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class IncidentsFragment : Fragment() {
+class IncidentsFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mapView: MapView
+    private lateinit var map: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_incidents, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_incidents, container, false)
+
+        // Initialize the MapView
+        mapView = rootView.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        return rootView
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // Add a marker for Almaty, Kazakhstan
+        val almatyLatLng = LatLng(43.2551, 76.9126)
+        val marker = MarkerOptions()
+            .position(almatyLatLng)
+            .title("Almaty City")
+            .snippet("Welcome to Almaty!")
+
+        map.addMarker(marker)
+
+        // Move the camera to Almaty
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(almatyLatLng, 12f))
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
 }
