86b7067bca82f099aeffcc90fbd3a404d0a12bc0
amiralimbekerov <asuraaka@outlook.com>
2024-03-14 23:51:47 +0500

    data class for api

---
+++ app/src/main/java/com/example/crimeguardian/data/Page.kt
@@ -0,0 +1,32 @@
+package com.example.crimeguardian.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Page(
+    @SerializedName("PageTitle")
+    val title: String,
+    @SerializedName("PageUrl")
+    val url: String,
+    @SerializedName("Count")
+    val count: Int,
+    @SerializedName("CountPercent")
+    val countPercent: Double,
+    @SerializedName("Diff")
+    val diff: Int,
+    @SerializedName("DiffPercent")
+    val diffPercent: Double,
+    @SerializedName("Read_end")
+    val readEnd: Int,
+    @SerializedName("Moved_on")
+    val movedOn: Int,
+    @SerializedName("Read")
+    val read: Read
+)
+
+data class Read(
+    @SerializedName("Minute")
+    val minute: Int,
+    @SerializedName("Second")
+    val second: Int
+)
+
