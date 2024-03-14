3794513211d16c00c172a803c24b466c60c63d0c
amiralimbekerov <asuraaka@outlook.com>
2024-03-14 23:51:36 +0500

    add retrofit library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     //Google Cloud and Map
     implementation ("com.google.android.gms:play-services-maps:18.1.0")
 
+    //API
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
 
