6c5c76f3df343b5183eca36aee75b2d989196001
amiralimbekerov <asuraaka@outlook.com>
2024-03-15 11:54:03 +0500

    add viewModel and livedata

---
+++ app/src/main/java/com/example/crimeguardian/NewsViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.crimeguardian
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.crimeguardian.api.Page
+import com.example.crimeguardian.module.ApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class NewsViewModel : ViewModel() {
+    private val apiClient = ApiClient()
+
+    private val _pageData = MutableLiveData<List<Page>>()
+    val pageData: LiveData<List<Page>> = _pageData
+
+    fun fetchDataFromApi() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val data = apiClient.getPageData()
+                withContext(Dispatchers.Main) {
+                    _pageData.value = data
+                }
+            } catch (e: Exception) {
+                // Handle error
+                e.printStackTrace()
+            }
+        }
+    }
+}
