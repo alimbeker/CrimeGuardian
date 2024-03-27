980dc12cecaa9e32e291f8444457a647753f462f
amiralimbekerov <asuraaka@outlook.com>
2024-03-27 22:05:36 +0500

    add BaseRepository

--- app/src/main/java/com/example/crimeguardian/data/mapper/ArticleMapper.kt
+++ app/src/main/java/com/example/crimeguardian/data/mapper/ArticleMapper.kt
@@ -1,9 +1,7 @@
 package com.example.crimeguardian.data.mapper
 
 import com.example.crimeguardian.data.model.ArticleDto
-import com.example.crimeguardian.data.model.NewsResponseDto
 import com.example.crimeguardian.presentation.model.model.Article
-import com.example.crimeguardian.presentation.model.model.NewsResponse
 
 internal fun ArticleDto.toArticle() = Article(
     title = title,
