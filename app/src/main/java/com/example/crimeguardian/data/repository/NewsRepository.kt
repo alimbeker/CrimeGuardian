55de7668ed09f74f502ebde63e2ca6ba801dbe62
amiralimbekerov <asuraaka@outlook.com>
2024-03-28 22:45:50 +0500

    delete unnecessary code

--- app/src/main/java/com/example/crimeguardian/data/repository/NewsRepository.kt
+++ app/src/main/java/com/example/crimeguardian/data/repository/NewsRepository.kt
@@ -1,17 +1,12 @@
 package com.example.crimeguardian.data.repository
 
-import android.util.Log
 import com.example.crimeguardian.core.BaseRepository
 import com.example.crimeguardian.core.functional.State
 import com.example.crimeguardian.data.api.NewsApi
 import com.example.crimeguardian.data.mapper.toNewsResponse
-import com.example.crimeguardian.data.model.NewsApiError
-import com.example.crimeguardian.data.model.NewsResponseDto
 import com.example.crimeguardian.presentation.model.model.NewsResponse
-import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
 
 
 class NewsRepository(private val api: NewsApi) : BaseRepository() {
