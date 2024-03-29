fac21af99d1bff5c2a6b2abfd9a6d3999ed787fe
amiralimbekerov <asuraaka@outlook.com>
2024-03-29 13:26:09 +0500

    add current location button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,6 @@ dependencies {
 
     //Google Cloud and Map
     implementation("com.google.android.gms:play-services-maps:18.1.0")
-
     //API
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
