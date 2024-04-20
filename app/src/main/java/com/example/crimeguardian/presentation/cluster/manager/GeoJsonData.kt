11f94a9e24d3338d17e062de6291bd0e2e111b8c
amiralimbekerov <asuraaka@outlook.com>
2024-04-20 22:56:32 +0500

    add data class for geoJson

---
+++ app/src/main/java/com/example/crimeguardian/presentation/cluster/manager/GeoJsonData.kt
@@ -0,0 +1,40 @@
+package com.example.crimeguardian.presentation.cluster.manager
+
+data class GeoJsonData(
+    val type: String,
+    val features: List<Feature>
+)
+
+data class Feature(
+    val type: String,
+    val geometry: Geometry,
+    val properties: Properties
+)
+
+data class Geometry(
+    val type: String,
+    val coordinates: List<Double>
+)
+
+data class Properties(
+    val dat_sover: String?,
+    val street: String?,
+    val stat: String?,
+    val time_period: String?,
+    val organ: String?,
+    val crime_code: String?,
+    val hard_code: String?,
+    val month: String?,
+    val year: String?,
+    val city_code: String?,
+    val ud: String?,
+    val longitude: String?,
+    val latitude: String?,
+    val objectid: String?,
+    val home_number: String?,
+    val reg_code: String?
+)
+
+
+
+
--- app/src/main/java/com/example/crimeguardian/presentation/viewmodel/IncidentsViewModel.kt
+++ app/src/main/java/com/example/crimeguardian/presentation/viewmodel/IncidentsViewModel.kt
@@ -5,14 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.crimeguardian.presentation.cluster.manager.GeoJsonData
 import com.example.crimeguardian.presentation.cluster.manager.MyClusterItem
 import com.google.android.gms.maps.model.LatLng
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.json.JSONArray
-import org.json.JSONObject
 import java.io.IOException
 
 class IncidentsViewModel : ViewModel() {
@@ -65,42 +64,4 @@ class IncidentsViewModel : ViewModel() {
         }
     }
 }
-
-data class GeoJsonData(
-    val type: String,
-    val features: List<Feature>
-)
-
-data class Feature(
-    val type: String,
-    val geometry: Geometry,
-    val properties: Properties
-)
-
-data class Geometry(
-    val type: String,
-    val coordinates: List<Double>
-)
-
-data class Properties(
-    val dat_sover: String?,
-    val street: String?,
-    val stat: String?,
-    val time_period: String?,
-    val organ: String?,
-    val crime_code: String?,
-    val hard_code: String?,
-    val month: String?,
-    val year: String?,
-    val city_code: String?,
-    val ud: String?,
-    val longitude: String?,
-    val latitude: String?,
-    val objectid: String?,
-    val home_number: String?,
-    val reg_code: String?
-)
-
-
-
 
