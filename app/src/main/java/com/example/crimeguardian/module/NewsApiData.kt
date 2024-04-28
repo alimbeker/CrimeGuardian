2051097eef52d173925955d14d58bec874b612d3
amiralimbekerov <asuraaka@outlook.com>
2024-04-28 22:46:25 +0500

    add NewsApiData in di

--- app/src/main/java/com/example/crimeguardian/module/NewsApiData.kt
+++ app/src/main/java/com/example/crimeguardian/module/NewsApiData.kt
@@ -2,13 +2,23 @@ package com.example.crimeguardian.module
 
 import com.example.crimeguardian.BuildConfig
 import com.example.crimeguardian.data.api.NewsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
+@Module
 object NewsApiData {
-    private fun getRetrofit(): Retrofit {
+
+    @Provides
+    @Singleton
+    fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .client(client)
             .baseUrl(BuildConfig.BASE_URL)
@@ -24,6 +34,8 @@ object NewsApiData {
         .addInterceptor(loggingInterceptor)
         .build()
 
+    @Provides
+    @Singleton
     fun getApi(): NewsApi {
         return getRetrofit()
             .create(NewsApi::class.java)
