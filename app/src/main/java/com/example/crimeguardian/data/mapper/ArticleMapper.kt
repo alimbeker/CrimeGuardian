ad410d83ed20fcc15eae0fddeeec3fc585a131e9
amiralimbekerov <asuraaka@outlook.com>
2024-04-30 21:06:06 +0500

    add new changes in mappers

--- app/src/main/java/com/example/crimeguardian/data/mapper/ArticleMapper.kt
+++ app/src/main/java/com/example/crimeguardian/data/mapper/ArticleMapper.kt
@@ -1,6 +1,8 @@
 package com.example.crimeguardian.data.mapper
 
 import com.example.crimeguardian.data.model.ArticleDto
+import com.example.crimeguardian.database.models.ArticleDBO
+import com.example.crimeguardian.database.models.Source
 import com.example.crimeguardian.presentation.model.model.news.Article
 
 internal fun ArticleDto.toArticle() = Article(
@@ -10,3 +12,27 @@ internal fun ArticleDto.toArticle() = Article(
     urlToImage = urlToImage ?: "",
     sourceName = source?.name ?: "",
 )
+
+internal fun ArticleDBO.toArticle(): Article {
+    return Article(
+        title = title ?: "",
+        description = description ?: "",
+        url = url ?: "",
+        urlToImage = urlToImage ?: "",
+        sourceName = source?.name ?: "",
+    )
+}
+
+internal fun ArticleDto.toArticleDbo(): ArticleDBO {
+    return ArticleDBO(
+        source = Source(id = source?.id, name = source?.name) ,
+        author = author ?: "",
+        title = title ?: "",
+        description = description ?: "",
+        url = url ?: "",
+        urlToImage = urlToImage ?: "",
+        publishedAt = publishedAt ?: "",
+        content = content ?: "",
+        id = 0
+    )
+}
