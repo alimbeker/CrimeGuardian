3db40b69de7e0e7c18f98ec6f946e1d5809de602
amiralimbekerov <asuraaka@outlook.com>
2024-05-08 21:06:51 +0500

    add new response news api

--- app/src/main/java/com/example/crimeguardian/data/api/NewsApi.kt
+++ app/src/main/java/com/example/crimeguardian/data/api/NewsApi.kt
@@ -2,11 +2,17 @@ package com.example.crimeguardian.data.api
 
 import com.example.crimeguardian.BuildConfig
 import com.example.crimeguardian.data.model.NewsResponseDto
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApi {
-
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("apiKey") key: String = BuildConfig.NEWS_API_KEY,
+        @Query("country") country: String,
+        @Query("category") category: String,
+    ) : NewsResponseDto
     @GET("everything")
     suspend fun getAllData(
         @Query("apiKey") key: String = BuildConfig.NEWS_API_KEY,
