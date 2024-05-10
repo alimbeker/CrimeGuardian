cb43366b39c3bdb248cc384ff60bf1b01fbe1fb0
amiralimbekerov <asuraaka@outlook.com>
2024-05-10 12:37:48 +0500

    changes

--- app/src/main/java/com/example/crimeguardian/DetailedFragment.kt
+++ app/src/main/java/com/example/crimeguardian/DetailedFragment.kt
@@ -1,59 +1,32 @@
 package com.example.crimeguardian
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.crimeguardian.databinding.FragmentDetailedBinding
+import com.example.crimeguardian.databinding.TopHeadlinesBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DetailedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class DetailedFragment : Fragment(R.layout.fragment_detailed) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentDetailedBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detailed, container, false)
+        binding = FragmentDetailedBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args = DetailedFragmentArgs.fromBundle(requireArguments())
+        binding.TvAuthor.text = args.sourceName
+        binding.TvHeading.text = args.title
+        binding.TvDescription.text = args.description
     }
 }
