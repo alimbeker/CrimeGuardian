48c3f41de35e7e9c786c77f9ee928004462ca3f1
amiralimbekerov <asuraaka@outlook.com>
2024-06-12 22:08:58 +0500

    change name of xml of main xml

--- app/src/main/java/com/example/crimeguardian/presentation/main/fragment/MainFragment.kt
+++ app/src/main/java/com/example/crimeguardian/presentation/main/fragment/MainFragment.kt
@@ -5,21 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.crimeguardian.databinding.ForFragmentMainBinding
 import com.example.crimeguardian.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
 
-    private var _binding: ForFragmentMainBinding? = null
+    private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = ForFragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
