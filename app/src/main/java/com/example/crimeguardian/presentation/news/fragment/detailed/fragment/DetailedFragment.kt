95a22b01451635ad3cf98befdc9e9aa624c33951
amiralimbekerov <asuraaka@outlook.com>
2024-06-26 01:48:01 +0500

    add back button in details in news

--- app/src/main/java/com/example/crimeguardian/presentation/news/fragment/detailed/fragment/DetailedFragment.kt
+++ app/src/main/java/com/example/crimeguardian/presentation/news/fragment/detailed/fragment/DetailedFragment.kt
@@ -1,10 +1,12 @@
-package com.example.crimeguardian.presentation.detailed.fragment
+package com.example.crimeguardian.presentation.news.fragment.detailed.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.crimeguardian.R
 import com.example.crimeguardian.databinding.FragmentDetailedBinding
@@ -29,8 +31,15 @@ class DetailedFragment : Fragment(R.layout.fragment_detailed) {
         binding.TvAuthor.text = args.sourceName
         binding.TvHeading.text = args.title
         binding.TvDescription.text = args.description
+
         Glide.with(requireContext())
             .load(args.urlToImage)
             .into(binding.IvBackground)
+
+        binding.IvBackButton.setOnClickListener {
+            Log.d("DetailedFragment", "Back button clicked")
+            findNavController().navigateUp()
+        }
+
     }
 }
